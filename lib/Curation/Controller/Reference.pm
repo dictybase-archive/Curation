1301a0aa42a5351cf15e8fa00d6903fd4ca8bcbe
Yulia Bushmanova <y.bushmanova@gmail.com>
2010-12-03 10:14:53 -0600

    disabled display of topic groups without topics, disabled display of 'artificial' pubmed ids

--- lib/Curation/Controller/Reference.pm
+++ lib/Curation/Controller/Reference.pm
@@ -106,7 +106,7 @@ sub show {
     $self->stash( title    => $ref->title ) if $ref->has_title;
     $self->stash( volume   => $ref->volume ) if $ref->has_volume;
     $self->stash( issue    => $ref->issue ) if $ref->has_issue;
-    $self->stash( pubmed   => $ref->pubmed_id ) if $ref->pubmed_id;
+    $self->stash( pubmed   => $ref->pubmed_id ) if $ref->pubmed_id && $ref->pubmed_id =~ m{^\d$};
     $self->stash( journal  => $ref->journal )
         if $ref->has_journal;
     $self->stash( linked => \@linked );
