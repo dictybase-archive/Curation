3ea11755dd0da069e17b57176e1f6972bdfcf964
Yulia Bushmanova <y.bushmanova@gmail.com>
2011-02-04 20:32:13 -0600

    replaced join on peragraph with 'exists' call

--- bin/autostats.sh
+++ bin/autostats.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-perl update_stats.pl -d=../db/stats.db -c=../conf/$MODE.yml
-perl dump_stats.pl -d=../db/stats.db -c=../conf/$MODE.yml -o=../data/stats.xls
+perl update_stats.pl -d=../db/stats.db -c=../conf/$MODE.yml && perl dump_stats.pl -d=../db/stats.db -c=../conf/$MODE.yml -o=../data/stats.xls
